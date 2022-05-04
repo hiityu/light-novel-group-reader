b23abe3bd615726cdc508ad1707233ac5cd3ec8d
Hiityu <james.stefanik@gmail.com>
2022-05-03 23:01:41 -0400

    further testing out webscrapping tools for a single novel site

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const puppeteer = require('puppeteer');
+const novelscrapper = require('./novelscrapper');
+(async () => {
+    await novelscrapper.initalize('master-of-martial-arts-has-an-advanced-optical-brain/');
+
+    let results = await novelscrapper.getResults(5);
+})();
