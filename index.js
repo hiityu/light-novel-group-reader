9ea8e380790ce613a4e288a667a80a4e54902e2b
Hiityu <james.stefanik@gmail.com>
2022-05-08 18:34:44 -0400

    base for next js

--- index.js
+++
@@ -1,6 +0,0 @@
-const novelscrapper = require('./novelscrapper');
-(async () => {
-    await novelscrapper.initalize('master-of-martial-arts-has-an-advanced-optical-brain/');
-
-    let results = await novelscrapper.getResults(5);
-})();
