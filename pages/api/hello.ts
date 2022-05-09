6ca4e0a2bdd503aa4ae02d237e970a4cd9e591b9
Hiityu <james.stefanik@gmail.com>
2022-05-08 23:15:22 -0400

    Basic on click load chapter page

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cheerio from 'cheerio'
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const scrape = async (req, res) => {
+  const inputValue = req.body.inputValue
+  const { data } = await axios.get(`https://boxnovel.com/novel/reincarnation-of-the-strongest-sword-god-boxnovel/chapter-${inputValue}/`)
+  const $ = cheerio.load(data)
+  const title = $(".text-left h1").text()
+  const body = $(".text-left p").text()
+  const lastScraped = new Date().toISOString()
+  res.status(200).json({ statusCode: 200,title:title ,body:body , lastScraped:lastScraped })
+} 
+export default scrape
